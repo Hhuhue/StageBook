0d9969077f21991d5b484d1bb32496041ceef40b
hhuhue <hhuhue@gmail.com>
2016-02-16 11:52:27 -0500

    Prêt pour publication

---
+++ symfony/src/PublicationBundle/Controller/PublicationController.php
@@ -0,0 +1,61 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: charl
+ * Date: 2016-02-16
+ * Time: 09:09
+ */
+
+namespace PublicationBundle\Controller;
+
+use PublicationBundle\Entity\Sujet;
+use PublicationBundle\Form\SujetType;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+
+
+class PublicationController extends Controller
+{
+    public function indexAction()
+    {
+        return $this->render("PublicationBundle:Pages:index.html.twig");
+    }
+
+    public function addAction(Request $request)
+    {
+        $sujet = new Sujet();
+
+        $form = $this->createForm(SujetType::class, $sujet);
+
+        if ($form->handleRequest($request)->isValid())
+        {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($sujet);
+            $em->flush();
+
+            return $this->redirect($this->generateUrl('publication_view_sujet', array('id' => $sujet->getId())));
+        }
+
+        return $this->render('PublicationBundle:Publication:Ajout.html.twig', array(
+            'form' => $form->createView(),
+        ));
+    }
+
+    public function viewSujetAction($id)
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $sujet = $em->getRepository("PublicationBundle:Sujet")->find($id);
+
+        return $this->render('PublicationBundle:Publication:Sujet.html.twig', array('sujet' => $sujet));
+    }
+
+    public function menuAction()
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $sujets = $em->getRepository("PublicationBundle:Sujet")->findAll();
+
+        return $this->render('PublicationBundle:Pages:menu.html.twig', array('sujets' => $sujets));
+    }
+}
---
+++ symfony/src/PublicationBundle/Entity/Sujet.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace PublicationBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Sujet
+ *
+ * @ORM\Table(name="sujet")
+ * @ORM\Entity(repositoryClass="PublicationBundle\Repository\SujetRepository")
+ */
+class Sujet
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="titre", type="string", length=255)
+     */
+    private $titre;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="contenu", type="text")
+     */
+    private $contenu;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set titre
+     *
+     * @param string $titre
+     *
+     * @return Sujet
+     */
+    public function setTitre($titre)
+    {
+        $this->titre = $titre;
+
+        return $this;
+    }
+
+    /**
+     * Get titre
+     *
+     * @return string
+     */
+    public function getTitre()
+    {
+        return $this->titre;
+    }
+
+    /**
+     * Set contenu
+     *
+     * @param string $contenu
+     *
+     * @return Sujet
+     */
+    public function setContenu($contenu)
+    {
+        $this->contenu = $contenu;
+
+        return $this;
+    }
+
+    /**
+     * Get contenu
+     *
+     * @return string
+     */
+    public function getContenu()
+    {
+        return $this->contenu;
+    }
+}
+
